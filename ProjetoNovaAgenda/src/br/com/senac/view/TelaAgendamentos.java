408a4a41b4ca7e45027ad23b6fdb012b3b9a3f16
Josielcosta138 <63210548+Josielcosta138@users.noreply.github.com>
2023-12-26 17:30:31 -0300

    Implementado Filtro Clientes na tela de Agendamentos

--- ProjetoNovaAgenda/src/br/com/senac/view/TelaAgendamentos.java
+++ ProjetoNovaAgenda/src/br/com/senac/view/TelaAgendamentos.java
@@ -8,6 +8,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumnModel;
 
@@ -30,6 +32,7 @@ import java.util.Date;
 import java.util.List;
 
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 
 import java.awt.Font;
@@ -43,8 +46,11 @@ import javax.persistence.criteria.Root;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
+import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
 import java.awt.Color;
+import javax.swing.JTextField;
 
 public class TelaAgendamentos extends JFrame {
 
@@ -53,6 +59,8 @@ public class TelaAgendamentos extends JFrame {
 	private JTable table_1;
 	private TableModel tableModel;
 	private CadastroPessoaView cadastroPessoaView;
+	private JList<String> clienteList;
+	private JFormattedTextField ftfCliente;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -77,7 +85,8 @@ public class TelaAgendamentos extends JFrame {
 			}
 		});
 
-		setIconImage(Toolkit.getDefaultToolkit().getImage(TelaAgendamentos.class.getResource("/br/com/senac/view/img/LogoSTYLEMANAGER black.png")));
+		setIconImage(Toolkit.getDefaultToolkit()
+				.getImage(TelaAgendamentos.class.getResource("/br/com/senac/view/img/LogoSTYLEMANAGER black.png")));
 		setTitle("EVENTOS - AGENDA");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -88,14 +97,20 @@ public class TelaAgendamentos extends JFrame {
 		contentPane.setLayout(null);
 		setLocationRelativeTo(null);
 
+		// Lista de Clientes na busca de campos
+		clienteList = new JList<>();
+		clienteList.setBounds(325, 49, 142, 141);
+		contentPane.add(clienteList);
+		clienteList.setVisible(false);
+
 		// Exibe a data e hora atuais no campo "Data e Hora"
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane.setBounds(23, 65, 942, 242);
+		scrollPane.setBounds(23, 60, 942, 242);
 		contentPane.add(scrollPane);
- 
+
 		tableModel = new TableModel();
 		tableModel.addColumn("Código");
 		tableModel.addColumn("Local");
@@ -106,9 +121,7 @@ public class TelaAgendamentos extends JFrame {
 		tableModel.addColumn("Cliente");
 		tableModel.addColumn("E-mail");
 		tableModel.addColumn("N°-Pessoas");
-		
-		
-		
+
 		table = new JTable(tableModel);
 		table.setAutoscrolls(true);
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
@@ -120,10 +133,9 @@ public class TelaAgendamentos extends JFrame {
 		tcm.getColumn(3).setPreferredWidth(140);
 		tcm.getColumn(4).setPreferredWidth(110);
 		tcm.getColumn(5).setPreferredWidth(120);
-		tcm.getColumn(6).setPreferredWidth(80);
+		tcm.getColumn(6).setPreferredWidth(110);
 		tcm.getColumn(7).setPreferredWidth(160);
 		tcm.getColumn(8).setPreferredWidth(60);
-		
 
 		scrollPane.setViewportView(table);
 
@@ -146,52 +158,86 @@ public class TelaAgendamentos extends JFrame {
 				listarAgendamentos();
 
 			}
-		}); 
+		});
 		btnPesquisar.setBounds(23, 318, 127, 23);
 		contentPane.add(btnPesquisar);
-		
+
 		JButton btnNewButton = new JButton("");
 		btnNewButton.setIcon(new ImageIcon(TelaAgendamentos.class.getResource("/br/com/senac/view/img/dateTime.png")));
 		btnNewButton.setBounds(1062, 31, 26, 23);
 		contentPane.add(btnNewButton);
-		
+
 		JButton btnVoltar = new JButton("Voltar");
 		btnVoltar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				if (cadastroPessoaView == null) {
 					cadastroPessoaView = new CadastroPessoaView();
 				}
 				cadastroPessoaView.setVisible(true);
 				dispose();
 			}
-				
+
 		});
-		btnVoltar.setIcon(new ImageIcon(TelaAgendamentos.class.getResource("/br/com/senac/view/img/2303132_arrow_back_direction_left_navigation_icon.png")));
+		btnVoltar.setIcon(new ImageIcon(TelaAgendamentos.class
+				.getResource("/br/com/senac/view/img/2303132_arrow_back_direction_left_navigation_icon.png")));
 		btnVoltar.setBounds(1100, 323, 100, 23);
 		contentPane.add(btnVoltar);
-		
+
 		JLabel lbliconePessoasAgend = new JLabel("");
-		lbliconePessoasAgend.setIcon(new ImageIcon(TelaAgendamentos.class.getResource("/br/com/senac/view/img/46832_group_search_icon.png")));
+		lbliconePessoasAgend.setIcon(new ImageIcon(
+				TelaAgendamentos.class.getResource("/br/com/senac/view/img/46832_group_search_icon.png")));
 		lbliconePessoasAgend.setBackground(new Color(192, 192, 192));
 		lbliconePessoasAgend.setBounds(23, 11, 60, 54);
 		contentPane.add(lbliconePessoasAgend);
-		
+
 		JLabel lblAgendaAberta = new JLabel("");
-		lblAgendaAberta.setIcon(new ImageIcon(TelaAgendamentos.class.getResource("/br/com/senac/view/img/AgendaAberta.png")));
+		lblAgendaAberta
+				.setIcon(new ImageIcon(TelaAgendamentos.class.getResource("/br/com/senac/view/img/AgendaAberta.png")));
 		lblAgendaAberta.setBounds(994, 70, 206, 235);
 		contentPane.add(lblAgendaAberta);
-		
+
 		JButton btnNewButton_1 = new JButton("Excluir");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				excluir();
-				
+
 			}
 		});
 		btnNewButton_1.setIcon(new ImageIcon(TelaAgendamentos.class.getResource("/br/com/senac/view/img/remove.png")));
 		btnNewButton_1.setBounds(160, 318, 113, 23);
 		contentPane.add(btnNewButton_1);
+
+		JLabel lblCliente = new JLabel("Cliente:");
+		lblCliente.setForeground(new Color(128, 128, 128));
+		lblCliente.setBounds(281, 35, 49, 14);
+		contentPane.add(lblCliente);
+		
+		// edição campo nome
+		clienteList.addListSelectionListener((ListSelectionListener) new ListSelectionListener() {
+					public void valueChanged(ListSelectionEvent e) {
+						if (!e.getValueIsAdjusting()) {
+							String selectedNome = clienteList.getSelectedValue();
+							if (selectedNome != null) {
+								ftfCliente.setText(selectedNome);
+								clienteList.setVisible(false);
+							}
+						}
+					}
+				});
+
+		JLabel lblData = new JLabel("Data:");
+		lblData.setForeground(new Color(128, 128, 128));
+		lblData.setBounds(562, 35, 46, 14);
+		contentPane.add(lblData);
+
+		JFormattedTextField ftfData = new JFormattedTextField();
+		ftfData.setBounds(597, 32, 158, 18);
+		contentPane.add(ftfData);
+
+		ftfCliente = new JFormattedTextField();
+		ftfCliente.setBounds(325, 32, 142, 18);
+		contentPane.add(ftfCliente);
 
 	}
 
@@ -228,82 +274,90 @@ public class TelaAgendamentos extends JFrame {
 				}
 			}
 		}
-		
-		
+
 	}
 
 	protected void listarAgendamentos() {
 		if (tableModel != null) {
 			tableModel.clearTable();
 		}
-		
+
 		BigInteger id = null;
 		Date dataHoraInicio = null;
 		Date dataHoraFim = null;
 		String local = null;
-		BigInteger participantes = null;
-		//List<BigInteger> participantes = null;
+ 		BigInteger participantes = null;
+		// List<BigInteger> participantes = null;
 		String status = null;
 		String tipoServico = null;
-		
-		
+		String nomeCliente = null;
+
 		try {
-			
-				try {
+
+			try {
+
 				
-					IService service = new Service();
-					System.out.println("******* Iniciando consulta de agendamentos *******");
-					EntityManager em = HibernateUtil.getEntityManager();
-
-					CriteriaBuilder cb = em.getCriteriaBuilder();
-					CriteriaQuery<EventoVO> criteria = cb.createQuery(EventoVO.class);
-
-					// Clausula from
-					Root<EventoVO> agendamentosFrom = criteria.from(EventoVO.class);
-					criteria.select(agendamentosFrom);
-
-				 
-					TypedQuery<EventoVO> query = em.createQuery(criteria);
-					//Order contatoOrderBy = cb.asc(agendamentosFrom.get("nome"));
-					//criteria.orderBy(contatoOrderBy);
-
-					List<EventoVO> listaAgendamentos = query.getResultList();
-					
-
-					System.out.println("Lista agendamentos --> "+listaAgendamentos);
-					//Collections.sort(listaAgendamentos, (contato1, contato2) -> contato1.getNome().compareTo(contato2.getNome()));
-//
-					for (EventoVO eventoVO : listaAgendamentos) {
-
-						if (eventoVO.getId() != null) {
-							System.out.println("Lista agendamentos --> "+listaAgendamentos);
-							RowData rowData = new RowData();
-							rowData.getValues().put(0, eventoVO.getId().toString());
-							rowData.getValues().put(1, eventoVO.getLocal());
-							rowData.getValues().put(2, eventoVO.getDataHoraInicio());
-							rowData.getValues().put(3, eventoVO.getDataHoraFim());
-							rowData.getValues().put(4, eventoVO.getStatus());
-							rowData.getValues().put(5, eventoVO.getTipoServico());
-							rowData.getValues().put(6, eventoVO.getNomeCliente());
-							rowData.getValues().put(7, eventoVO.getEmail());
-							rowData.getValues().put(8, eventoVO.getParticipantes());
-							
-
-							rowData.setElement(eventoVO);
-							tableModel.addRow(rowData);
-						}else {
-							JOptionPane.showMessageDialog(null, "Sem Agendamentos no momento!", null, JOptionPane.WARNING_MESSAGE);
-						}
-						
-					}
-					
-					
-				} catch (Exception e) {
-					throw new BOValidationException("Código: erro de validação" + " valor inválido");
+				if (this.ftfCliente.getText() != null && ftfCliente.getText().trim().length() > 0) {
+					nomeCliente = ftfCliente.getText().trim();
 				}
-			
-			
-			
+
+				if (this.ftfCliente.getText() != null && ftfCliente.getText().trim().length() > 0) {
+					nomeCliente = ftfCliente.getText().trim();
+				}
+				   
+				
+				System.out.println("******* Iniciando consulta de agendamentos *******");
+				EntityManager em = HibernateUtil.getEntityManager();
+
+				CriteriaBuilder cb = em.getCriteriaBuilder();
+				CriteriaQuery<EventoVO> criteria = cb.createQuery(EventoVO.class);
+
+				// Clausula from
+				Root<EventoVO> agendamentosFrom = criteria.from(EventoVO.class);
+				criteria.select(agendamentosFrom);
+				
+				
+				if (nomeCliente != null) {
+					String searchTerm = "%" + nomeCliente.toLowerCase() + "%";
+					criteria.where(cb.like(cb.lower(agendamentosFrom.get("nomeCliente")), searchTerm));
+				} 
+
+				
+				TypedQuery<EventoVO> query = em.createQuery(criteria);
+				List<EventoVO> listaAgendamentos = query.getResultList();
+
+				System.out.println("Lista agendamentos --> " + listaAgendamentos);
+				
+				Collections.sort(listaAgendamentos, (cliente1, cliente2) -> cliente1.getNomeCliente().compareTo(cliente2.getNomeCliente()));
+
+				for (EventoVO eventoVO : listaAgendamentos) {
+
+					if (eventoVO.getId() != null) {
+						System.out.println("Lista agendamentos --> " + listaAgendamentos);
+						RowData rowData = new RowData();
+						rowData.getValues().put(0, eventoVO.getId().toString());
+						rowData.getValues().put(1, eventoVO.getLocal());
+						rowData.getValues().put(2, eventoVO.getDataHoraInicio());
+						rowData.getValues().put(3, eventoVO.getDataHoraFim());
+						rowData.getValues().put(4, eventoVO.getStatus());
+						rowData.getValues().put(5, eventoVO.getTipoServico());
+						rowData.getValues().put(6, eventoVO.getNomeCliente());
+						rowData.getValues().put(7, eventoVO.getEmail());
+						rowData.getValues().put(8, eventoVO.getParticipantes());
+
+						rowData.setElement(eventoVO);
+						tableModel.addRow(rowData);
+					} else {
+						JOptionPane.showMessageDialog(null, "Sem Agendamentos no momento!", null,
+								JOptionPane.WARNING_MESSAGE);
+					}
+
+				}
+
+			} catch (Exception e) {
+				throw new BOValidationException("Código: erro de validação" + " valor inválido");
+			}
+
 		} catch (BOValidationException e) {
 			e.printStackTrace();
 			JOptionPane.showMessageDialog(this, e.getMessage(), "Erro de validação", JOptionPane.WARNING_MESSAGE);
